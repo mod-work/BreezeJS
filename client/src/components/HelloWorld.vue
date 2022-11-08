2e8c2a586a05cafaca0d021c7da3784b760c282b
Shihab Chy <cshihab04@gmail.com>
2022-11-08 17:18:45 +0600

    clearing template code

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -13,10 +13,10 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
+    <!-- <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    </p> -->
   </div>
 
   <p>
